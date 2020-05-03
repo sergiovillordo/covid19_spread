40da488c651eece56336e04d5fc869aff92c52a5
ser08 <sergiomanuel03@gmail.com>
2020-05-03 17:45:10 +0200

    initial commit

---
+++ download_data.sh
@@ -0,0 +1,3 @@
+wget https://covid.ourworldindata.org/data/ecdc/full_data.csv -O data/world/full_data.csv
+wget https://covid19.isciii.es/resources/serie_historica_acumulados.csv -O data/spain/serie_historica_acumulados.csv 
+wget --no-check-certificate -O data/argentina/Covid19arData_historico.csv  'https://docs.google.com/spreadsheets/d/16-bnsDdmmgtSxdWbVMboIHo5FRuz76DBxsz_BbsEVWA/export?format=csv&id=16-bnsDdmmgtSxdWbVMboIHo5FRuz76DBxsz_BbsEVWA&gid=0&format=csv'
